2efeb1e55dc3f879b949f04612245c30bfa4360b
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-06-24 15:28:54 +0200

    added difference in the authority registry

--- normarc-to-marc21-test.py
+++ normarc-to-marc21-test.py
@@ -143,6 +143,7 @@ def compare(identifier, normarc_path, marc21_path):
         normarc_line = normarc_line.replace("Båstad, Babbis Friis", "Baastad, Babbis Friis")
         normarc_line = normarc_line.replace("Saint Exupéry, Antoine de", "Saint-Exupéry, Antoine de")
         normarc_line = normarc_line.replace("Vesås, Tarjei", "Vesaas, Tarjei")
+        normarc_line = normarc_line.replace("Åsen, Arne Påsche", "Aasen, Arne Paasche")
 
         # The definition of "adult" has changed from 17+ in NORMARC to 18+ in MARC21
         if normarc_line == '<meta property="typicalAgeRange">17-</meta>':
