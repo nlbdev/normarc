e29eda40ffdfc44f646ba592c566cf8f9779645b
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-07-01 13:46:14 +0200

    add 9275 to list of strange series conversions

--- normarc-to-marc21-test.py
+++ normarc-to-marc21-test.py
@@ -269,7 +269,7 @@ def compare(identifier, normarc_path, marc21_path):
                     marc21_offset += 1
                     continue
             
-            if identifier in ["9115"]:
+            if identifier in ["9115", "9275"]:
                 # strange conversion of series metadata, skip for now
                 if "*440" in normarc_line_comment or "*490" in normarc_line_comment or "*830" in normarc_line_comment:
                     normarc_offset += 1
