dfa8e361e5f54b5e9f98488b0727a638b6e2744f
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-08-10 13:21:55 +0200

    skip checking the number of pages and volumes for the braille newsletter

--- normarc-to-marc21-test.py
+++ normarc-to-marc21-test.py
@@ -384,6 +384,15 @@ def compare(identifier, normarc_path, marc21_path, normarc_source_path, marc21_s
                     marc21_skip_lines.append(f"MARC21: skipped line {marc21_linenum+1} (reason #20): {marc21_line}")
                     break
 
+            # skip checking the number of pages and volumes for the braille newsletter
+            if identifier in ["120209"]:
+                if "*300" in marc21_line_comment:
+                    marc21_skip_lines.append(f"MARC21: skipped line {marc21_linenum+1} (reason #21): {marc21_line}")
+                    continue
+                if "*300" in normarc_line_comment:
+                    normarc_skip_lines.append(f"NORMARC: skipped line {normarc_linenum+1} (reason #22): {normarc_line}")
+                    continue
+
             # refines attribute names differ when there is both a *440 and a *490 in NORMARC, so just ignore the numbering in those cases
             normarc_line = re.sub(r'(refines="#series-title)-\d+', r'\1-X', normarc_line)
             marc21_line = re.sub(r'(refines="#series-title)-\d+', r'\1-X', marc21_line)
