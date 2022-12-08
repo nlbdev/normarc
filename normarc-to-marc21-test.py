b0720778fee9d5880945290668aa2938b6c010b8
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-12-08 02:00:11 +0100

    dewey in *699$1 is not converted to MARC21

--- normarc-to-marc21-test.py
+++ normarc-to-marc21-test.py
@@ -1344,6 +1344,11 @@ def compare(identifier, normarc_path, marc21_path, normarc_source_path, marc21_s
                 normarc_skip_lines.append(f"NORMARC: skipped line {normarc_linenum+1} (reason #38.6): {normarc_line}")
                 continue
             
+            # Dewey in *699$1 is not converted to MARC21 (example: 115410)
+            if normarc_line_property == "dc:subject.dewey" and "*699$1" in normarc_line_comment:
+                normarc_skip_lines.append(f"NORMARC: skipped line {normarc_linenum+1} (reason #38.6): {normarc_line}")
+                continue
+            
             # # Nationality in *800$j is not converted to MARC21
             # if "*800$j" in normarc_line_comment:
             #     normarc_skip_lines.append(f"NORMARC: skipped line {normarc_linenum+1} (reason #39): {normarc_line}")
